324fb5bb2bbacf397c30c586057af1a406b89bd8
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-11-15 22:46:46 +0300

    todo

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,6 +3,7 @@ import { RootState } from "../app/store";
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
+  //!todo add required role for route protecton and authorization
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
